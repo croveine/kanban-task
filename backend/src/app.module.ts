d59db8692d0ae333be3c95d78976c3c8cb39e80b
croveine <volodymyr.zvirko.ki.2020@lpnu.ua>
2025-06-16 18:53:46 +0300

    fix: add Node.js type references

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
